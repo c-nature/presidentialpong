67eac0a318f87e41db309dd9500059ea601f4973
Your GitHub Username <your_github_email@example.com>
2025-07-25 23:54:45 -0400

    fix

--- presidentialpong.service.worker.js
+++ presidentialpong.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1753320872|92207197930';
+const CACHE_VERSION = '1753502032|542003759';
 /** @type {string} */
 const CACHE_PREFIX = 'ponggame-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
